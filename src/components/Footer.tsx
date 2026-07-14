57d3e5f47918559c77720c4de4e88fd7135f90fb
mazzuccowow-web <mazzuccowow@gmail.com>
2026-07-14 19:10:48 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@
 
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { Mail, Phone, MapPin, Facebook, Linkedin } from 'lucide-react';
+import { Mail, Phone, MapPin } from 'lucide-react';
 
 export default function Footer() {
   const { t } = useTranslation();
@@ -58,12 +58,24 @@ export default function Footer() {
           </ul>
         </div>
 
-        {/* COLONNA 4: SOCIAL MEDIA (Icone con i colori ufficiali di brand) */}
+        {/* COLONNA 4: SOCIAL MEDIA (Con i loghi ufficiali a colori pieni) */}
         <div>
           <h4 className="text-brand-gold font-serif text-lg mb-6">{t('footer.socialMedia')}</h4>
           <ul className="space-y-4 text-sm opacity-80 font-light">
+            
+            {/* FACEBOOK */}
             <li className="flex items-center space-x-3">
-              <Facebook className="w-4 h-4 text-[#1877F2]" /> {/* Blu ufficiale Facebook */}
+              {/* Logo ufficiale Facebook: cerchio blu con "f" bianca */}
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 24 24" 
+                className="w-5 h-5 flex-shrink-0"
+              >
+                <path 
+                  fill="#1877F2" 
+                  d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+                />
+              </svg>
               <a 
                 href="https://www.facebook.com/salentopropertyprojects?locale=en_GB" 
                 target="_blank" 
@@ -73,8 +85,20 @@ export default function Footer() {
                 Facebook
               </a>
             </li>
+
+            {/* LINKEDIN */}
             <li className="flex items-center space-x-3">
-              <Linkedin className="w-4 h-4 text-[#0A66C2]" /> {/* Blu ufficiale LinkedIn */}
+              {/* Logo ufficiale LinkedIn: quadrato blu con "in" bianco */}
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 24 24" 
+                className="w-5 h-5 flex-shrink-0"
+              >
+                <path 
+                  fill="#0A66C2" 
+                  d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0z"
+                />
+              </svg>
               <a 
                 href="https://www.linkedin.com/company/salento-property-projects/?viewAsMember=true" 
                 target="_blank" 
@@ -84,6 +108,7 @@ export default function Footer() {
                 LinkedIn
               </a>
             </li>
+
           </ul>
         </div>
 
